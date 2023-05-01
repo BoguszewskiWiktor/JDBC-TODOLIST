da1b3e9a831a925ab4cd6d7cc9df2f6ed124551a
Wiktor Boguszewski <wiktor.boguszewski06@gmail.com>
2023-05-01 12:46:37 +0200

    My first commit

---
+++ src/pl/zajavka/CommandBuilder.java
@@ -0,0 +1,75 @@
+package pl.zajavka;
+
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CommandBuilder {
+
+    Optional<Command> buildCommand(final String line) {
+        String[] split = line.split(";");
+        String commandType = split[0];
+        if (!Command.Type.valuesAsList().contains(commandType)) {
+            System.err.printf("User provided unknown command: [%s]%n", commandType);
+            return Optional.empty();
+        }
+
+        List<String> stringCommandWithParamExtracted = Arrays.asList(split).subList(1, split.length);
+
+        Map<String, String> parametersMap = stringCommandWithParamExtracted.stream()
+                .map(item -> item.split("="))
+//                .filter(paramSplit -> !ToDoItem.Field.SORT.name().equals(paramSplit[0]))
+                .collect(Collectors.toMap(itemSplit -> itemSplit[0], itemSplit -> itemSplit[1]));
+
+        List<String> sortingParams = Optional.ofNullable(parametersMap.get(ToDoItem.Field.SORT.name()))
+                .map(params -> List.of(params.split(",")))
+                .orElse(Collections.emptyList());
+
+        ToDoItem toDoItem = buildToDoItem(parametersMap);
+        return Optional.of(new Command(
+                Command.Type.from(commandType),
+                toDoItem,
+                findSortingField(sortingParams),
+                findSortingDir(sortingParams)
+        ));
+    }
+
+    private ToDoItem.Field findSortingField(List<String> sortingParams) {
+        if (sortingParams.isEmpty()) {
+            return ToDoItem.Field.NAME;
+        }
+
+        try {
+            return ToDoItem.Field.valueOf(sortingParams.get(0));
+        } catch (Exception e) {
+            System.err.printf("Sorting field is not specified. Default: [%s]%n", ToDoItem.Field.NAME);
+            return ToDoItem.Field.NAME;
+        }
+    }
+
+    private Command.SortDir findSortingDir(List<String> sortingParams) {
+        if (sortingParams.isEmpty()) {
+            return Command.SortDir.ASC;
+        }
+
+        try {
+            return Command.SortDir.valueOf(sortingParams.get(1));
+        } catch (Exception e) {
+            System.err.printf("Sorting dir is not specified. Default: [%s]%n", Command.SortDir.ASC);
+            return Command.SortDir.ASC;
+        }
+    }
+
+    private ToDoItem buildToDoItem(final Map<String, String> parametersMap) {
+        ToDoItem toDoItem = new ToDoItem();
+        Optional.ofNullable(parametersMap.get(ToDoItem.Field.NAME.name()))
+                .ifPresent(toDoItem::setName);
+        Optional.ofNullable(parametersMap.get(ToDoItem.Field.DESCRIPTION.name()))
+                .ifPresent(toDoItem::setDescription);
+        Optional.ofNullable(parametersMap.get(ToDoItem.Field.DEADLINE.name()))
+                .ifPresent(deadline -> toDoItem.setDeadline(LocalDateTime.parse(deadline, ToDoItem.DATE_FORMAT)));
+        Optional.ofNullable(parametersMap.get(ToDoItem.Field.PRIORITY.name()))
+                .ifPresent(priority -> toDoItem.setPriority(Integer.valueOf(priority)));
+        return toDoItem;
+    }
+}
---
+++ src/pl/zajavka/DatabaseRunner.java
@@ -0,0 +1,226 @@
+package pl.zajavka;
+
+import java.sql.*;
+import java.util.*;
+import java.util.function.Consumer;
+
+public class DatabaseRunner {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/todo";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String SQL_INSERT =
+            "INSERT INTO TODO (NAME, DESCRIPTION, DEADLINE, PRIORITY) VALUES (?, ?, ?, ?);";
+    private static final String SQL_UPDATE =
+            "UPDATE TODO SET DESCRIPTION = ?, DEADLINE = ?, PRIORITY = ? WHERE NAME = ?;";
+    private static final String SQL_READ_WHERE = "SELECT * FROM TODO WHERE NAME = ?;";
+    private static final String SQL_READ_ALL = "SELECT * FROM TODO ORDER BY ?1 ?2";
+    private static final String SQL_GROUPED =
+            "SELECT DATE(deadline) AS DATE, ARRAY_AGG(name) AS TASKS FROM TODO GROUP BY DATE(deadline) ORDER BY DATE;";
+    private static final String SQL_DELETE = "DELETE FROM TODO WHERE NAME = ?;";
+    private static final String SQL_DELETE_ALL = "DELETE FROM TODO;";
+
+    private final Map<Command.Type, Consumer<Command>> EXECUTION_MAP;
+
+    {
+        EXECUTION_MAP = Map.of(
+                Command.Type.CREATE, this::runAdd,
+                Command.Type.UPDATE, this::runUpdate,
+                Command.Type.READ, this::runRead,
+                Command.Type.READ_ALL, this::runReadAll,
+                Command.Type.READ_GROUPED, this::runGrouped,
+                Command.Type.DELETE, this::runDelete,
+                Command.Type.DELETE_ALL, this::runDeleteAll
+        );
+    }
+
+    void run(final Command command) {
+        System.out.println("##### RUNNING COMMAND #####");
+        Consumer<Command> commandConsumer = Optional.ofNullable(EXECUTION_MAP.get(command.getType()))
+                .orElseThrow(() -> new IllegalArgumentException(
+                        String.format("Command: [%S] not supported", command.getType())));
+
+        commandConsumer.accept(command);
+        System.out.println("##### FINISHED COMMAND #####\n");
+    }
+
+    private void runAdd(final Command command) {
+        if (!Command.Type.CREATE.equals(command.getType())) {
+            throw new IllegalArgumentException(command.getType().getName());
+        }
+
+        try (
+                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                PreparedStatement statement = connection.prepareStatement(SQL_INSERT)
+        ) {
+            statement.setString(1, command.getToDoItem().getName());
+            statement.setString(2, command.getToDoItem().getDescription());
+            statement.setTimestamp(3, Timestamp.valueOf(command.getToDoItem().getDeadline()));
+            statement.setInt(4, command.getToDoItem().getPriority());
+            int count = statement.executeUpdate();
+            System.out.printf("Run: [%s] successfully, inserted: [%s]%n", command.getType(), count);
+        } catch (SQLException e) {
+            System.err.printf("[%s] error. Message: [%s]%n", command.getType(), e.getMessage());
+        }
+    }
+
+    private void runUpdate(final Command command) {
+        if (!Command.Type.UPDATE.equals(command.getType())) {
+            throw new IllegalArgumentException(command.getType().getName());
+        }
+
+        try (
+                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                PreparedStatement statement = connection.prepareStatement(SQL_UPDATE)
+        ) {
+            statement.setString(1, command.getToDoItem().getDescription());
+            statement.setTimestamp(2, Timestamp.valueOf(command.getToDoItem().getDeadline()));
+            statement.setInt(3, command.getToDoItem().getPriority());
+            statement.setString(4, command.getToDoItem().getName());
+            int count = statement.executeUpdate();
+            System.out.printf("Run: [%s] successfully, updated: [%s]%n", command.getType(), count);
+        } catch (SQLException e) {
+            System.err.printf("[%s] data error. Message: [%s]%n", command.getType(), e.getMessage());
+        }
+    }
+
+    private void runRead(final Command command) {
+        if (!Command.Type.READ.equals(command.getType())) {
+            throw new IllegalArgumentException(command.getType().getName());
+        }
+
+        try (
+                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                PreparedStatement statement = connection.prepareStatement(SQL_READ_WHERE)
+        ) {
+            statement.setString(1, command.getToDoItem().getName());
+            try (ResultSet resultSet = statement.executeQuery()) {
+                List<ToDoItem> readItems = mapToToDo(resultSet);
+                print(readItems);
+                System.out.printf("Run: [%s] successfully, updated: [%s]%n", command.getType(), readItems.size());
+            }
+        } catch (SQLException e) {
+            System.err.printf("[%s] data error. Message: [%s]%n", command.getType(), e.getMessage());
+        }
+    }
+
+    private void runReadAll(final Command command) {
+        if (!Command.Type.READ_ALL.equals(command.getType())) {
+            throw new IllegalArgumentException(command.getType().getName());
+        }
+
+        try (
+                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                PreparedStatement statement = connection.prepareStatement(SQL_READ_ALL
+                        .replace("?1", command.getSortBy().name())
+                        .replace("?2", command.getSortDir().name()))
+        ) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                List<ToDoItem> readItems = mapToToDo(resultSet);
+                print(readItems);
+                System.out.printf("Run: [%s] successfully, updated: [%s]%n", command.getType(), readItems.size());
+            }
+        } catch (SQLException e) {
+            System.err.printf("[%s] data error. Message: [%s]%n", command.getType(), e.getMessage());
+        }
+    }
+
+    private void runGrouped(final Command command) {
+        if (!Command.Type.READ_GROUPED.equals(command.getType())) {
+            throw new IllegalArgumentException(command.getType().getName());
+        }
+
+        try (
+                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                PreparedStatement statement = connection.prepareStatement(SQL_GROUPED)
+        ) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                var grouped = mapToGrouped(resultSet);
+                print(grouped);
+                System.out.printf("Run: [%s] successfully, updated: [%s]%n", command.getType(), grouped.size());
+            }
+        } catch (SQLException e) {
+            System.err.printf("[%s] data error. Message: [%s]%n", command.getType(), e.getMessage());
+        }
+    }
+
+    private Map<String, String> mapToGrouped(ResultSet resultSet) throws SQLException {
+        Map<String, String> result = new LinkedHashMap<>();
+        while (resultSet.next()) {
+            result.put(resultSet.getString("DATE"), resultSet.getString("TASKS"));
+        }
+        return result;
+    }
+
+    private List<ToDoItem> mapToToDo(ResultSet resultSet) throws SQLException {
+        List<ToDoItem> result = new ArrayList<>();
+        while (resultSet.next()) {
+            ToDoItem toDoItem = new ToDoItem();
+            toDoItem.setName(resultSet.getString(ToDoItem.Field.NAME.name()));
+            toDoItem.setDescription(resultSet.getString(ToDoItem.Field.DESCRIPTION.name()));
+            toDoItem.setDeadline(resultSet.getTimestamp(ToDoItem.Field.DEADLINE.name()).toLocalDateTime());
+            toDoItem.setPriority(resultSet.getInt(ToDoItem.Field.PRIORITY.name()));
+            result.add(toDoItem);
+        }
+        return result;
+    }
+
+    private void print(List<ToDoItem> readItems) {
+        System.out.println("PRINTING TO DO LIST");
+        String schema = "%-25s%-25s%-25s%-25s%n";
+        System.out.printf(schema,
+                ToDoItem.Field.NAME.name(),
+                ToDoItem.Field.DESCRIPTION.name(),
+                ToDoItem.Field.DEADLINE.name(),
+                ToDoItem.Field.PRIORITY.name());
+        readItems.forEach(
+                entry -> System.out.printf(
+                        schema,
+                        entry.getName(),
+                        entry.getDescription(),
+                        entry.getDeadline(),
+                        entry.getPriority()
+                ));
+    }
+
+    private void print(Map<String, String> items) {
+        System.out.println("READ GROUPED");
+        String schema = "%-25s%-25s%n";
+        System.out.printf(schema, "DATE", "TASKS");
+        for (var entry : items.entrySet()) {
+            System.out.printf(schema, entry.getKey(), entry.getValue());
+        }
+    }
+
+    private void runDelete(final Command command) {
+        if (!Command.Type.DELETE.equals(command.getType())) {
+            throw new IllegalArgumentException(command.getType().getName());
+        }
+
+        try (
+                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                PreparedStatement statement = connection.prepareStatement(SQL_DELETE)
+        ) {
+            statement.setString(1, command.getToDoItem().getName());
+            int count = statement.executeUpdate();
+            System.out.printf("Run: [%s] successfully, deleted: [%s]%n", command.getType(), count);
+        } catch (SQLException e) {
+            System.err.printf("[%s] data error. Message: [%s]%n", command.getType(), e.getMessage());
+        }
+    }
+    private void runDeleteAll(final Command command) {
+        if (!Command.Type.DELETE_ALL.equals(command.getType())) {
+            throw new IllegalArgumentException(command.getType().getName());
+        }
+
+        try (
+                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                PreparedStatement statement = connection.prepareStatement(SQL_DELETE_ALL)
+        ) {
+            int count = statement.executeUpdate();
+            System.out.printf("Run: [%s] successfully, deleted: [%s]%n", command.getType(), count);
+        } catch (SQLException e) {
+            System.err.printf("[%s] data error. Message: [%s]%n", command.getType(), e.getMessage());
+        }
+    }
+}
